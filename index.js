b6b6eca2644778612f9674122f2c89e9b1751f46
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-06 16:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,14 +22,10 @@ function divide (a, b) {
 }
 
 function inc(n) {
-  return ()
+  return a += 1 
 }
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
 })
-
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-}) 
 
